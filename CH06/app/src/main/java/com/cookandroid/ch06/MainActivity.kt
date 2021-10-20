0edd0bc47829e3f20a4eea8a15a2bfabc729efdd
Inwoo Jeong <sinth92@hotmail.com>
2021-10-20 15:52:37 +0900

    액션바, 프래그먼트 callback 오류

--- CH06/app/src/main/java/com/cookandroid/ch06/MainActivity.kt
+++ CH06/app/src/main/java/com/cookandroid/ch06/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.cookandroid.ch06
 
-import android.app.TabActivity
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.ActionBar
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
-//@Suppress("deprecation")
-class MainActivity : AppCompatActivity() {
+@Suppress("deprecation")
+class MainActivity : AppCompatActivity(), ActionBar.TabListener {
+//class MainActivity : AppCompatActivity() {
 //class MainActivity : TabActivity() {
+
+    lateinit var tabSong : ActionBar.Tab
+    lateinit var tabArtist : ActionBar.Tab
+    lateinit var tabAlbum : ActionBar.Tab
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /*setContentView(R.layout.activity_main)
@@ -64,5 +76,73 @@ class MainActivity : AppCompatActivity() {
 
         //setContentView(R.layout.ch06_14)
 
+        title = "액션바와 프레그먼트"
+        var bar = this.supportActionBar
+        bar!!.navigationMode = ActionBar.NAVIGATION_MODE_TABS
+
+        tabSong = bar.newTab()
+        tabSong.text = "음악별"
+        tabSong.setTabListener(this)
+        bar.addTab(tabSong)
+
+        tabArtist = bar.newTab()
+        tabArtist.text = "가수별"
+        tabSong.setTabListener(this)
+        bar.addTab(tabArtist)
+
+        tabAlbum = bar.newTab()
+        tabAlbum.text = "앨범별"
+        tabAlbum.setTabListener(this)
+        bar.addTab(tabAlbum)
+
     }
+
+    class MyTabFragment : Fragment(){
+        var tabName : String? = null
+        override fun onCreate(savedInstanceState: Bundle?){
+            super.onCreate(savedInstanceState)
+            var data = arguments
+            tabName = data!!.getString("tabName")
+        }
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            var params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT)
+
+            var baseLayout = LinearLayout(super.getActivity())
+            baseLayout.orientation = LinearLayout.VERTICAL
+            baseLayout.layoutParams = params
+
+            if(tabName === "음악별") baseLayout.setBackgroundColor(Color.RED)
+            if(tabName === "가수별") baseLayout.setBackgroundColor(Color.GREEN)
+            if(tabName === "앨범별") baseLayout.setBackgroundColor(Color.BLUE)
+
+            return baseLayout
+        }
+    }
+
+    var myFrags = arrayOfNulls<MyTabFragment>(3)
+
+    override fun onTabSelected(tab: ActionBar.Tab, ft: FragmentTransaction) {
+        var myTabFrag : MyTabFragment? = null
+
+        if(myFrags[tab.position]==null){
+            myTabFrag = MyTabFragment()
+            val data = Bundle()
+            data.putString("tabName", tab.text.toString())
+            myTabFrag.arguments = data
+            myFrags[tab.position] = myTabFrag
+        }else{
+            myTabFrag = myFrags[tab.position]
+        }
+        ft.replace(android.R.id.content, myTabFrag!!)
+    }
+
+    override fun onTabUnselected(tab: ActionBar.Tab, ft: FragmentTransaction) {  }
+
+    override fun onTabReselected(tab: ActionBar.Tab, ft: FragmentTransaction) {  }
 }
