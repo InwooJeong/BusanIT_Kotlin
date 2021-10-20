c592b06ae88fa6b1ec66e1bdb3c6b44ec36a5d34
Inwoo Jeong <sinth92@hotmail.com>
2021-10-20 14:34:30 +0900

    바, 스크롤뷰, 뷰플리퍼, 탭호스트

--- CH06/app/src/main/java/com/cookandroid/ch06/MainActivity.kt
+++ CH06/app/src/main/java/com/cookandroid/ch06/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.cookandroid.ch06
 
+import android.app.TabActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.MultiAutoCompleteTextView
+import android.widget.*
 
+//@Suppress("deprecation")
 class MainActivity : AppCompatActivity() {
+//class MainActivity : TabActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        /*setContentView(R.layout.activity_main)
+        title="자동완성"
         var items = arrayOf("CSI-뉴욕", "CSI-라스베가스", "CSI-마이애미",
                             "Friends", "Fringe", "Lost")
 
@@ -22,7 +23,46 @@ class MainActivity : AppCompatActivity() {
         var multi = findViewById<MultiAutoCompleteTextView>(R.id.multiAutoCompleteTextView1)
         var token = MultiAutoCompleteTextView.CommaTokenizer()
         multi.setTokenizer(token)
-        multi.setAdapter(adapter)
+        multi.setAdapter(adapter)*/
+
+        /*setContentView(R.layout.ch06_15)
+        title="뷰 플리퍼"
+        var btnPrev : Button
+        var btnNext : Button
+        var vFlipper : ViewFlipper
+
+        btnPrev = findViewById<Button>(R.id.btnPrev)
+        btnNext = findViewById<Button>(R.id.btnNext)
+        vFlipper = findViewById<ViewFlipper>(R.id.viewFlipper1)
+
+        btnPrev.setOnClickListener{
+            vFlipper.showPrevious()
+        }
+
+        btnNext.setOnClickListener{
+            vFlipper.showNext()
+        }*/
+        
+        /*setContentView(R.layout.ch06_17)
+        title="탭 호스트"
+
+        var tabHost = this.tabHost
+
+        var tabSpecSong = tabHost.newTabSpec("SONG").setIndicator("음악별")
+        tabSpecSong.setContent(R.id.tabSong)
+        tabHost.addTab(tabSpecSong)
+
+        var tabSpecArtist = tabHost.newTabSpec("ARTIST").setIndicator("가수별")
+        tabSpecArtist.setContent(R.id.tabArtist)
+        tabHost.addTab(tabSpecArtist)
+
+        var tabSpecAlbum = tabHost.newTabSpec("ALBUM").setIndicator("앨범별")
+        tabSpecAlbum.setContent(R.id.tabAlbum)
+        tabHost.addTab(tabSpecAlbum)
+
+        tabHost.currentTab = 0*/
+
+        //setContentView(R.layout.ch06_14)
 
     }
 }
