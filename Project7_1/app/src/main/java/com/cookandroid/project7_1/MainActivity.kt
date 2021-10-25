be48faf49522f0065afbdf4f0be1ebc06dcf757e
Inwoo Jeong <sinth92@hotmail.com>
2021-10-25 11:30:21 +0900

    xml -> kotlin

--- Project7_1/app/src/main/java/com/cookandroid/project7_1/MainActivity.kt
+++ Project7_1/app/src/main/java/com/cookandroid/project7_1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.SubMenu
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
@@ -16,20 +17,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "배경색 바꾸기"
+        /*title = "배경색 바꾸기 XML"*/
+        title = "배경색 바꾸기 Kotlin"
         baseLayout = findViewById<LinearLayout>(R.id.baseLayout)
         button1 = findViewById<Button>(R.id.button1)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
-        var mInflater = menuInflater
-        mInflater.inflate(R.menu.menu1, menu)
+        /*var mInflater = menuInflater
+        mInflater.inflate(R.menu.menu1, menu)*/
+
+        menu!!.add(0, 1, 0, "배경색 (빨강)")
+        menu!!.add(0, 2, 0, "배경색 (초록)")
+        menu!!.add(0, 3, 0, "배경색 (파랑)")
+
+        var sMenu : SubMenu = menu.addSubMenu("버튼 변경 >>")
+        sMenu.add(0, 4, 0, "버튼 45도 회전")
+        sMenu.add(0, 5, 0, "버튼 2배 확대")
+
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        /*when(item.itemId){
             R.id.itemRed -> {
                 baseLayout.setBackgroundColor(Color.RED)
                 return true
@@ -47,6 +58,29 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             R.id.subSize -> {
+                button1.scaleX = 2f
+                return true
+            }
+        }*/
+
+        when(item.itemId){
+            1->{
+                baseLayout.setBackgroundColor(Color.RED)
+                return true
+            }
+            2->{
+                baseLayout.setBackgroundColor(Color.GREEN)
+                return true
+            }
+            3->{
+                baseLayout.setBackgroundColor(Color.BLUE)
+                return true
+            }
+            4->{
+                button1.rotation = 45f
+                return true
+            }
+            5->{
                 button1.scaleX = 2f
                 return true
             }
