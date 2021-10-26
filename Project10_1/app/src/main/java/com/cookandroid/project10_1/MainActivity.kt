6a9f7f8c9fd84e6e7832a7a8179806129f7f9cc7
Inwoo Jeong <sinth92@hotmail.com>
2021-10-26 14:07:52 +0900

    10-1 참고

--- Project10_1/app/src/main/java/com/cookandroid/project10_1/MainActivity.kt
+++ Project10_1/app/src/main/java/com/cookandroid/project10_1/MainActivity.kt
@@ -26,6 +26,22 @@ class MainActivity : AppCompatActivity() {
             }else{
                 startActivity(thirdIntent)
             }
+
+           /* var btn1 = findViewById<Button>(R.id.btnNewActivity)
+            var rdo2 = findViewById<RadioButton>(R.id.rdoSecond)
+            var rdo3 = findViewById<RadioButton>(R.id.rdoThird)
+
+            btn1.setOnClickListener {
+
+                var in1: Intent? = null
+                if(rdo2.isChecked)
+                    in1 = Intent(applicationContext, SecondActivity::class.java)
+                else if(rdo3.isChecked)
+                    in1 = Intent(applicationContext, ThirdActivity::class.java)
+                else
+                    Toast.makeText(applicationContext, "선택하숑~",  Toast.LENGTH_SHORT).show()
+
+                startActivity(in1)*/
         }
     }
 }
