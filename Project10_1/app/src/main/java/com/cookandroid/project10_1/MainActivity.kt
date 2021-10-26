4195980053eed1241fa7dcd76dc87788505e07fc
Inwoo Jeong <sinth92@hotmail.com>
2021-10-26 11:49:46 +0900

    3 activities

--- Project10_1/app/src/main/java/com/cookandroid/project10_1/MainActivity.kt
+++ Project10_1/app/src/main/java/com/cookandroid/project10_1/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.cookandroid.project10_1
 
 import android.content.Intent
+import android.icu.text.CaseMap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title="3 Activities"
 
+        var r2nd = findViewById<RadioButton>(R.id.r2nd)
         var btnNewActivity = findViewById<Button>(R.id.btnNewActivity)
+
         btnNewActivity.setOnClickListener{
-            var intent = Intent(applicationContext, SecondActivity::class.java)
-            startActivity(intent)
+            var secondIntent = Intent(applicationContext, SecondActivity::class.java)
+            var thirdIntent = Intent(applicationContext, ThirdActivity::class.java)
+
+            if(r2nd.isChecked){
+                startActivity(secondIntent)
+            }else{
+                startActivity(thirdIntent)
+            }
         }
     }
 }
---
+++ Project10_1/app/src/main/java/com/cookandroid/project10_1/ThirdActivity.kt
@@ -0,0 +1,18 @@
+package com.cookandroid.project10_1
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+
+class ThirdActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.third)
+
+        var btnReturn = findViewById<Button>(R.id.btnReturn2)
+        btnReturn.setOnClickListener{
+            finish()
+        }
+    }
+}
