a7c85088e0825ee8345f12c9551f77432423886b
Inwoo Jeong <sinth92@hotmail.com>
2021-10-26 15:14:27 +0900

    최다 득표 그림 나오게 변경

--- Project10_2/app/src/main/java/com/cookandroid/project10_2/ResultActivity.kt
+++ Project10_2/app/src/main/java/com/cookandroid/project10_2/ResultActivity.kt
@@ -3,6 +3,7 @@ package com.cookandroid.project10_2
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
 
@@ -21,10 +22,19 @@ class ResultActivity : Activity() {
         var rbar = arrayOfNulls<RatingBar>(imageName.size)
 
         var tvID = arrayOf(R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5,
-                           R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9)
+                            R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9)
 
         var rbarID = arrayOf(R.id.rbar1, R.id.rbar2, R.id.rbar3, R.id.rbar4,
-                           R.id.rbar5, R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9)
+                            R.id.rbar5, R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9)
+
+        var imageFileID = arrayOf(R.drawable.pic1, R.drawable.pic2, R.drawable.pic3,
+                            R.drawable.pic4, R.drawable.pic5, R.drawable.pic6,
+                            R.drawable.pic7, R.drawable.pic8, R.drawable.pic9)
+
+        var tvWin = findViewById<TextView>(R.id.tvWin)
+        var ivWin = findViewById<ImageView>(R.id.ivWin)
+
+        var cnt : Int = 0
 
         for(i in voteResult!!.indices){
             tv[i] = findViewById<TextView>(tvID[i])
@@ -35,6 +45,15 @@ class ResultActivity : Activity() {
             tv[i]!!.setText(imageName.get(i))
             rbar[i]!!.setRating(voteResult[i].toFloat())
         }
+
+        for(i in 0..8){
+            if(voteResult[cnt]<voteResult[i]){
+                cnt = i;
+            }
+        }
+
+        tvWin.setText(imageName[cnt])
+        ivWin.setImageResource(imageFileID[cnt])
 
         var btnReturn = findViewById<Button>(R.id.btnReturn)
         btnReturn.setOnClickListener{
