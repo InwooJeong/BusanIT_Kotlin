aa59688e7dbe2af31e6ea3b32eb11b5f34b91936
Inwoo Jeong <sinth92@hotmail.com>
2021-10-27 15:27:45 +0900

    Spinner

--- CH11/app/src/main/java/com/cookandroid/ch11/MainActivity.kt
+++ CH11/app/src/main/java/com/cookandroid/ch11/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "리스트뷰 테스트"
+        //title = "리스트뷰 테스트"
 
         /*var mid = arrayOf("히어로즈", "24시", "로스트", "로스트룸", "스몰빌", "탐정몽크", "빅뱅이론",
                         "프렌즈", "덱스터", "글리", "가쉽걸", "테이큰", "슈퍼내추럴", "브이")
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(applicationContext, mid[position], Toast.LENGTH_SHORT).show()
         }*/
 
-        var midList = ArrayList<String>()
+        /*var midList = ArrayList<String>()
         var list = findViewById<ListView>(R.id.listView1)
 
         var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, midList)
@@ -45,6 +45,18 @@ class MainActivity : AppCompatActivity() {
             midList.removeAt(position)
             adapter.notifyDataSetChanged()
             false
-        }
+        }*/
+        
+        title = "스피너 테스트"
+
+        var movie = arrayOf("쿵푸팬더", "짱구는 못말려", "아저씨",
+                "아바타", "대부", "국가대표", "토이스토리3",
+                "마당을 나온 암탉", "죽은 시인의 사회", "서유기")
+
+        var spinner = findViewById<Spinner>(R.id.spinner1)
+
+        var adapter: ArrayAdapter<String>
+        adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, movie)
+        spinner.adapter = adapter
     }
 }
