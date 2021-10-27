0313d345b68ff17ef165cd5fa0acef6f43f9fcb4
Inwoo Jeong <sinth92@hotmail.com>
2021-10-27 15:20:25 +0900

    toast

--- Project11_2/app/src/main/java/com/cookandroid/project11_2/MainActivity.kt
+++ Project11_2/app/src/main/java/com/cookandroid/project11_2/MainActivity.kt
@@ -5,11 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.Gallery
-import android.widget.ImageView
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toastText : TextView
+    lateinit var toastView : View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +26,11 @@ class MainActivity : AppCompatActivity() {
             R.drawable.mov11, R.drawable.mov12, R.drawable.mov13, R.drawable.mov14,
             R.drawable.mov15, R.drawable.mov16, R.drawable.mov17, R.drawable.mov18,
             R.drawable.mov19, R.drawable.mov20
+        )
+
+        var movieName = arrayOf(
+            "여인의 향기", "쥬리기 공원", "포레스트 검프", "사랑의 블랙홀", "혹성탈출 : 진화의 시작",
+            "아름다운 비행", "내 이름은 칸", "해리포터 : 죽음의 성물2", "마더", "킹콩을 들다"
         )
 
         override fun getCount(): Int {
@@ -50,6 +56,15 @@ class MainActivity : AppCompatActivity() {
                 var ivPoster = findViewById<ImageView>(R.id.ivPoster)
                 ivPoster.scaleType = ImageView.ScaleType.FIT_CENTER
                 ivPoster.setImageResource(posterID[position])
+                //Toast.makeText(applicationContext, "토스트", Toast.LENGTH_SHORT).show()
+
+                var toast = Toast(this@MainActivity)
+                toastView = View.inflate(this@MainActivity, R.layout.toast1,null)
+                toastText = toastView.findViewById<TextView>(R.id.toastText1)
+                toastText.text = movieName[position]
+                toast.view = toastView
+                toast.show()
+
                 false
             }
 
