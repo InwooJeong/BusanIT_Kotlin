42f73f4bcba288ab22e4fb881db0228a6f18775b
Inwoo Jeong <sinth92@hotmail.com>
2021-10-27 15:01:45 +0900

    기본 갤러리뷰

--- Project11_2/app/src/main/java/com/cookandroid/project11_2/MainActivity.kt
+++ Project11_2/app/src/main/java/com/cookandroid/project11_2/MainActivity.kt
@@ -3,8 +3,11 @@ package com.cookandroid.project11_2
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.Gallery
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +21,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     inner class MyGalleryAdapter(var context: Context) : BaseAdapter(){
-        var PosterID = arrayOf(
-            
+        var posterID = arrayOf(
+            R.drawable.mov11, R.drawable.mov12, R.drawable.mov13, R.drawable.mov14,
+            R.drawable.mov15, R.drawable.mov16, R.drawable.mov17, R.drawable.mov18,
+            R.drawable.mov19, R.drawable.mov20
         )
+
+        override fun getCount(): Int {
+            return posterID.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return 0
+        }
+
+        override fun getItemId(position: Int): Long {
+            return 0
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var imageview = ImageView(context)
+            imageview.layoutParams = Gallery.LayoutParams(200,300)
+            imageview.scaleType = ImageView.ScaleType.FIT_CENTER
+            imageview.setPadding(5, 5, 5, 5)
+            imageview.setImageResource(posterID[position])
+
+            imageview.setOnTouchListener{ v, event ->
+                var ivPoster = findViewById<ImageView>(R.id.ivPoster)
+                ivPoster.scaleType = ImageView.ScaleType.FIT_CENTER
+                ivPoster.setImageResource(posterID[position])
+                false
+            }
+
+            return imageview
+        }
+
     }
 }
