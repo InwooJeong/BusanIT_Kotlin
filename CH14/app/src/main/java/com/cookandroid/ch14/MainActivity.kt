d5dfb7c80249ddaa6906db6e7ee584cc9c77e05a
Inwoo Jeong <sinth92@hotmail.com>
2021-10-28 12:17:38 +0900

    Call log

--- CH14/app/src/main/java/com/cookandroid/ch14/MainActivity.kt
+++ CH14/app/src/main/java/com/cookandroid/ch14/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.cookandroid.ch14
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CallLog
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var soundIntent : Intent
+    /*lateinit var soundIntent : Intent
     lateinit var btnStart : Button
-    lateinit var btnStop : Button
+    lateinit var btnStop : Button*/
+    lateinit var btnCall : Button
+    lateinit var edtCall : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "서비스 테스트 예제"
+        /*title = "서비스 테스트 예제(개선)"
 
         soundIntent = Intent(this, MusicService::class.java)
         btnStart = findViewById<Button>(R.id.btnStart)
@@ -21,13 +31,54 @@ class MainActivity : AppCompatActivity() {
 
         btnStart.setOnClickListener{
             startService(soundIntent)
+            finish()
             android.util.Log.i("서비스 테스트", "startService()")
+            //Toast.makeText(applicationContext, "startService()", Toast.LENGTH_SHORT).show()
         }
 
         btnStop.setOnClickListener{
             stopService(soundIntent)
             android.util.Log.i("서비스 테스트", "stopService()")
-        }
+            //Toast.makeText(applicationContext, "stopService()", Toast.LENGTH_SHORT).show()
+        }*/
 
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.READ_CALL_LOG), Context.MODE_PRIVATE)
+
+        btnCall = findViewById<Button>(R.id.btnCall)
+        edtCall = findViewById<EditText>(R.id.edtCall)
+        btnCall.setOnClickListener{
+            edtCall.setText(findCallHistory())
+        }
+    }
+
+    fun findCallHistory():String{
+        var callSet = arrayOf(CallLog.Calls.DATE, CallLog.Calls.TYPE,
+            CallLog.Calls.NUMBER, CallLog.Calls.DURATION)
+
+        var c = contentResolver.query(CallLog.Calls.CONTENT_URI,
+            callSet, null, null, null)
+
+        if(c!!.count==0)
+            return "통화기록 없음"
+        var callBuff = StringBuffer() //최대 100 통화 저장
+
+        callBuff.append("\n날짜 : 구분 : 전화번호 : 통화시간\n\n")
+        c.moveToFirst()
+        do{
+            var callDate = c.getLong(0)
+            var datePattern = SimpleDateFormat("yyyy-MM-dd")
+            var date_str = datePattern.format(Date(callDate))
+            callBuff.append("$date_str:")
+            if(c.getInt(1)==CallLog.Calls.INCOMING_TYPE)
+                callBuff.append("착신 : ")
+            else
+                callBuff.append("발신 : ")
+            callBuff.append(c.getString(2) + ":")
+            callBuff.append(c.getString(3) + "초\n")
+        }while (c.moveToNext())
+
+        c.close()
+        return callBuff.toString()
     }
 }
--- CH14/app/src/main/java/com/cookandroid/ch14/MusicService.kt
+++ CH14/app/src/main/java/com/cookandroid/ch14/MusicService.kt
@@ -3,6 +3,7 @@ package com.cookandroid.ch14
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.IBinder
+import android.widget.Toast
 
 class MusicService : android.app.Service(){
     lateinit var mp : MediaPlayer
@@ -13,6 +14,7 @@ class MusicService : android.app.Service(){
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         android.util.Log.i("서비스 테스트", "onStartCommand()")
+        //Toast.makeText(applicationContext, "onStartCommand()", Toast.LENGTH_SHORT).show()
         mp = MediaPlayer.create(this,R.raw.song1)
         mp.isLooping = true
         mp.start()
@@ -21,11 +23,13 @@ class MusicService : android.app.Service(){
 
     override fun onCreate() {
         android.util.Log.i("서비스 테스트", "onCreate()")
+        //Toast.makeText(applicationContext, "onCreate()", Toast.LENGTH_SHORT).show()
         return super.onCreate()
     }
 
     override fun onDestroy() {
         android.util.Log.i("서비스 테스트", "onDestroy()")
+        //Toast.makeText(applicationContext, "onDestroy()", Toast.LENGTH_SHORT).show()
         mp.stop()
         super.onDestroy()
     }
